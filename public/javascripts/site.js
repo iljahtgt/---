e1830ffe512e85257528541a264750bcc85dfd35
iljahtgt <74045995+iljahtgt@users.noreply.github.com>
2021-08-04 17:28:31 +0800

    Delete public directory

--- public/javascripts/site.js
+++
@@ -1,7 +0,0 @@
-
-      function closeNav(){
-          document.getElementById("mySideBar").style.width="0";
-      }
-      function openNav(){
-          document.getElementById("mySideBar").style.width="150px";
-      }
