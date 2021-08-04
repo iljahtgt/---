bda11519439c18efa835011b5906aa36348d4c70
iljahtgt <74045995+iljahtgt@users.noreply.github.com>
2021-08-04 17:28:19 +0800

    Delete models directory

--- models/Product.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
-var Schema=mongoose.Schema;
-var ProductSchema=new Schema({
-    title:String,
-    price:Number,
-    quantity:Number
-},{collection:"AIEN12Collection"});
-
-module.exports=mongoose.model('Product',ProductSchema);
